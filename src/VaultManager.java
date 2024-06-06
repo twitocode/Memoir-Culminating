ac5a714a39b05a50181a9340bfd2974a8e7fe524
twitocode <twitodev@gmail.com>
2024-06-06 13:13:45 -0400

    Commenting

--- src/VaultManager.java
+++ src/VaultManager.java
@@ -1,15 +1,16 @@
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VaultManager {
 
     public static List<Vault> vaults = new ArrayList<Vault>();
-    public static Vault currentVault;
+    private static Vault _current;
 
-    public static Vault findVault(String name) {
+    public static Vault findVaultByName(String name) {
         for (var v : vaults) {
-            if (v.name == name) {
+            if (v.name.equals(name)) {
                 return v;
             }
         }
@@ -17,12 +18,50 @@ public class VaultManager {
         return null;
     }
 
-    public static void setCurrentVault(String name) {
+    public static void setCurrentVaultByName(String name) {
         for (var v : vaults) {
-            if (v.name == name) {
-                currentVault = v;
+            if (v.name.equals(name)) {
+                _current = v;
                 return;
             }
         }
+    }
+
+    public static void setCurrentVault(Vault vault) {
+        _current = vault;
+    }
+
+    public static void addVault(String name) {
+        vaults.add(new Vault(name));
+    }
+
+    public static JournalFile findNoteByName(String name) {
+        for (var f : _current.files) {
+            if (f.name.equals(name)) {
+                return f;
+            }
+        }
+
+        return null;
+    }
+
+    public static String getName() {
+        return _current.name;
+    }
+
+    public static Vault get() {
+        return _current;
+    }
+
+    public static List<JournalFile> getFiles() {
+        return _current.files;
+    }
+
+    public static void loadFiles() throws IOException {
+        _current.loadFiles();
+    }
+
+    public static void addFile(JournalFile file) {
+        _current.files.add(file);
     }
 }
